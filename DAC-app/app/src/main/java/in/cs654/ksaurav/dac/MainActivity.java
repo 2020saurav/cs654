da2d4b0fea23471f5b96a21078d4fdd50cc46402
Saurav Kumar <2020saurav@gmail.com>
2016-01-11 18:54:46 +0530

    Calculator xml layout

---
+++ DAC-app/app/src/main/java/in/cs654/ksaurav/dac/MainActivity.java
@@ -0,0 +1,13 @@
+package in.cs654.ksaurav.dac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
