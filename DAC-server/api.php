257a60a2b984edf83e2a6c7180b07b3d86741d68
Saurav Kumar <2020saurav@gmail.com>
2016-01-13 00:40:04 +0530

    simple dac api for evaluation

---
+++ DAC-server/api.php
@@ -0,0 +1,27 @@
+<?php
+
+$input    = utf8_decode(urldecode($_POST["input"]));
+error_log($input);
+
+// TODO caching using MySQL or file
+if (checkSanity($input)) {
+    try {
+        eval( '$result = (' . $input . ');' );
+        if ($result) {
+            $response = $result;
+        } else {
+            $response = "Bad Input";
+        }
+    } catch (Exception $e) {
+        $response = "Bad Input";
+    }
+    echo json_encode($response);
+} else {
+    $response = "Bad Input";
+    echo json_encode($response);
+}
+
+// TODO see if it can be made more secure
+function checkSanity($string) {
+    return preg_match('/^[0-9|.|+|-|*|\/]*$/', $string);
+}
